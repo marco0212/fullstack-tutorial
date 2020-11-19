b2b4c2ca81073e26817c53f4dddfe0ecd35677fe
Jeong <inyeop0212@gmail.com>
2020-11-20 00:05:16 +0900

    Build a schema

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,1 +1,13 @@
 require('dotenv').config();
+
+const { ApolloServer } = require('apollo-server');
+const typeDefs = require('./schema');
+
+const server = new ApolloServer({ typeDefs });
+
+server.listen().then(() => {
+  console.log(`
+    Server is running!
+    Listening on port 4000 🧖‍♂️
+  `);
+});
